e23f5c92da7368a99b4353a8199f7cfc89aac98c
Olnair2932 <olnairpereira@gmail.com>
2026-06-06 14:58:45 -0300

    Limpeza: Removendo backups, testes e arquivos de segurança

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,10 @@
-const CACHE_NAME = 'bash-search-v2';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/config.js',
-  '/comandos/lista_cmd_bash.txt',
-  '/manifest.json',
-  '/icons/icon.svg'
-];
+const CACHE_NAME = 'bash-v2';
+const assets = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icons/icon.svg'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
